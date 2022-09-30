d9e3c51e31bd8c8b328949fff53ef8b101eec0cc
Pedro Suga <108696298+pedrosuga@users.noreply.github.com>
2022-09-30 12:27:35 -0300

    Add files via upload

---
+++ divisao2float.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float x = 2;
+    float y = 5.53;
+    float resultado = y / x;
+    printf("Resultado: %.2f", resultado);
+    return 0;
+}
