d69a1c2816918c5c31b3bb137d43e42ab5a8f058
Pedro Suga <108696298+pedrosuga@users.noreply.github.com>
2022-09-30 14:31:43 -0300

    Add files via upload

---
+++ Condicionais_Ex2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x, y;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &x);
+    printf("Digite o segundo valor: ");
+    scanf("%d", &y);
+
+    soma = x + y;
+
+    if(x > y){
+        printf("O primeiro valor eh maior que o segundo");
+    }
+    else if (x < y){
+        printf("O segundo valor eh maior que o primeiro")
+    }
+    else{
+        printf("O primeiro e o segundo sao iguais");
+    }
+}
