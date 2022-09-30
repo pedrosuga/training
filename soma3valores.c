5d11b67c0a66fd6fb4ca4516f2d3015fdfc5ba0c
Pedro Suga <108696298+pedrosuga@users.noreply.github.com>
2022-09-30 12:00:21 -0300

    Add files via upload

---
+++ soma3valores.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int resultado = a + b + c;
+    printf("Resultado: %d", resultado);
+    return 0;
+}
