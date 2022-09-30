a91111bc384de5d1eb31b8589a27616ac1104665
Pedro Suga <108696298+pedrosuga@users.noreply.github.com>
2022-09-30 12:27:03 -0300

    Add files via upload

---
+++ multiplicacao2float.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float x = 0.5;
+    float y = 0.5;
+    float resultado = x * y;
+    printf("Resultado: %f", resultado);
+    return 0;
+}
