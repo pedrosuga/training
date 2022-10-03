28d2499254170b16bd3ddbe6ffea5b8ee31ecb4d
Pedro Suga <108696298+pedrosuga@users.noreply.github.com>
2022-10-03 17:07:57 -0300

    Add files via upload

---
+++ Laço Repetição For_Ex1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i;
+    int soma = 0;
+
+    for(int i = 0; i < 10; i++){
+
+        //Print ii and skip a line
+        printf ("i = %d (Soma: %d) \n", i, soma);
+
+        /* Increment the value of i in the sum
+        Sum gets the current value of sum + i */
+        soma = soma + i;
+        
+    }
+    
+    printf("O valor da soma eh: %d", soma);
+    return 0;
+}
